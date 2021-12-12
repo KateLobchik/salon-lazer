04d6f730147054146e646d33f1feabdd6a11434d
KateLobch <elobchevskaya@diasoft.ru>
2021-12-13 01:08:30 +0300

    Adaptive style to map and footer (part 1)

--- source/js/map.js
+++ source/js/map.js
@@ -7,3 +7,17 @@ if (imgMap) {
   imgMap.classList.remove('contacts-map--active');
   interactiveMap.classList.add('contacts-map--active');
 }
+
+
+
+const mapButton = document.querySelectorAll('.adress__adress-item');
+const mapIcon = document.querySelectorAll('.adress__map-icon');
+const mapBlock = document.querySelectorAll('.contacts__map-wrapper');
+
+for (let i = 0; i < mapButton.length; i++) {
+  mapButton[i].addEventListener('click', () => {
+    console.log('vnkjv')
+    mapIcon[i].classList.toggle('adress__map-icon--active');
+    mapBlock[i].classList.toggle('contacts__map-wrapper--active');
+  })
+}
