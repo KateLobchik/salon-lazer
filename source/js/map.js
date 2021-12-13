c57cdf118e2d8c649bd12488019ddb526a3ec70e
KateLobch <elobchevskaya@diasoft.ru>
2021-12-13 23:10:42 +0300

    Adaptive style to map (index) + fixed bugs

--- source/js/map.js
+++ source/js/map.js
@@ -1,23 +1,32 @@
+//Map
+const interactiveMap = document.querySelectorAll('.contacts-map--interactive');
 
-  //Map
-const interactiveMap = document.querySelector('.contacts-map--interactive');
-const imgMap = document.querySelector('.contacts-map--img');
-
-if (imgMap) {
-  imgMap.classList.remove('contacts-map--active');
-  interactiveMap.classList.add('contacts-map--active');
-}
-
-
-
-const mapButton = document.querySelectorAll('.adress__adress-item');
+const mapButton = document.querySelectorAll('.adress__map-input');
 const mapIcon = document.querySelectorAll('.adress__map-icon');
+
+const mapIvanCity = document.querySelector('.contacts__map-ivancity');
+const mapKingisep = document.querySelector('.contacts__map-kingisep');
 const mapBlock = document.querySelectorAll('.contacts__map-wrapper');
 
+
+
 for (let i = 0; i < mapButton.length; i++) {
+
+    interactiveMap[0].classList.add('contacts-map--active');
+
   mapButton[i].addEventListener('click', () => {
-    console.log('vnkjv')
-    mapIcon[i].classList.toggle('adress__map-icon--active');
-    mapBlock[i].classList.toggle('contacts__map-wrapper--active');
+
+    for (let ind = 0; ind < mapIcon.length; ind++) {
+      mapIcon[ind].classList.remove('adress__map-icon--active');
+      mapBlock[ind].classList.remove('contacts__map-wrapper--active');
+      interactiveMap[ind].classList.remove('contacts-map--active');
+    }
+
+    if (`${mapButton[i]}:checked`) {
+      mapIcon[i].classList.add('adress__map-icon--active');
+      mapBlock[i].classList.add('contacts__map-wrapper--active');
+      interactiveMap[i].classList.add('contacts-map--active');
+  }
+
   })
 }
